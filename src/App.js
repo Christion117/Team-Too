52b1b0b913529e0e39e840dca82497de19afcfd7
jlloyd647 <jlloyd647@gmail.com>
2022-10-17 20:15:56 -0400

    First push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0)
+
+  const increase = () => {
+    setNumber(number + 1)
+  }
+
+  const reduce = () => {
+    setNumber(number - 1)
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Team Too!
-        </a>
-      </header>
+       <div>
+        <div className="count">
+          <h3>Count:</h3>
+          <h1>{ number }</h1>
+        </div>
+        <div className="buttons">
+          <button title={"-"} onClick={increase}> Add </button>
+          <button title={"+"} onClick={reduce}> Subtract </button>
+        </div>
+      </div>
     </div>
   );
 }
